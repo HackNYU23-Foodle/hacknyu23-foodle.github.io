b39e993c56c4873b62d46952e23149aebba61b79
Calvin <48587962+calvinatian@users.noreply.github.com>
2023-02-19 05:33:54 -0500

    CHG image path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/Foodle/' : '',
+  assetPrefix: isProd ? '/Foodle/' : '/Foodle',
   images: {
     unoptimized: true,
   },
